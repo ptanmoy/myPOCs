777e22c40123b8f0fde68a398a7382515a31e987
Tanmoy Palodhi <tanmoypalodhi@outlook.com>
2018-08-24 00:52:24 -0700

    Updated text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('That's an automated Heroku Deployment! Great One!!')
+  response.send('That's an automated Heroku Deployment! Great One!!!')
 })
 
 app.listen(app.get('port'), function() {
